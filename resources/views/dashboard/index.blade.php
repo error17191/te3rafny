ce243fc621372ba7304ec2d969125131d699ae74
Mohamed Ahmed <error17191@gmail.com>
2018-06-13 17:58:26 +0200

    Prototyped Dashboard Design

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,15 +5,23 @@
 @section('content')
     <div class="container" id="dashboard">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="jumbotron">
-                    <h1 class="display-4">Hello, world!</h1>
-                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                    <hr class="my-4">
-                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                    </p>
+            <div class="col-md-10 col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <nav class="nav nav-pills nav-justified">
+                            <a class="nav-item nav-link active" href="#">New</a>
+                            <a class="nav-item nav-link" href="#">Public</a>
+                            <a class="nav-item nav-link" href="#">Private</a>
+                        </nav>
+                        <br>
+                        <h1 class="display-4">Hello, world!</h1>
+                        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <hr class="my-4">
+                        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                        <p class="lead">
+                            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
